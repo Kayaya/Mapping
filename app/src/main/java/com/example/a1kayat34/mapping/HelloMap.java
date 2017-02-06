1e603f2f48bd15aee15dbbb87bc0eda2f6f6fe89
1kayat34 <1kayat34@solent.ac.uk>
2017-02-06 12:55:53 +0000

    Added new menu and layout

--- app/src/main/java/com/example/a1kayat34/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1kayat34/mapping/HelloMap.java
@@ -3,10 +3,19 @@ package com.example.a1kayat34.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import android.content.Intent;
 
 
 public class HelloMap extends Activity {
@@ -29,5 +38,40 @@ public class HelloMap extends Activity {
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+
+        //Button go = (Button) findViewById(R.id.go);
+       //go.setOnClickListener(this);
     }
+   /* //@Override
+    public void onClick(View view){
+        EditText latitude = (EditText) findViewById(R.id.latitude);
+        EditText longitude = (EditText) findViewById(R.id.longitude);
+
+        double lat = Double.parseDouble(latitude.getText().toString());
+        double lon = Double.parseDouble(longitude.getText().toString());
+
+        mv.getController().setCenter(new GeoPoint(lat,lon));
+    }*/
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            // react to the menu item being selected...
+            //System.exit(0);
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+
+
 }
